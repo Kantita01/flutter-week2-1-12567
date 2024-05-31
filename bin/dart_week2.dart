9cdddb89c9b73f6e933087c441923b00180d8954
Kantita01 <kantita5069@gmail.com>
2024-05-31 19:05:08 +0700

    first commit

---
+++ bin/dart_week2.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+void main(List<String> argrument) {
+  inputIfelse();
+  // conIfEsle();
+  // conIf();
+}
+
+void inputIfelse(){
+  int age;
+  print("Enter number:");
+  age = int.parse(stdin.readLineSync()!);
+  if(age > 25){
+    print('Age:$age is True');
+  } else {
+    print('Age:$age is False');
+  }
+
+}
+
+void conIfEsle() {
+  const age = 25;
+  if (age == 20){
+    print('Age:$age is True');
+  } else {
+    print('Age:$age is False');
+  }
+}
+
+
+void conIf(){
+  const age = 25;
+  if (age == 25){
+    print('Age:$age');
+  }
+}
